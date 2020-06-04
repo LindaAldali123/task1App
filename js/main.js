9fe133d753939dde5cd8dac15b9cc425eebbe6ed
LindaAldali123 <eng.lindaaldali123@gmail.com>
2020-06-05 01:03:44 +0300

    firstjson

--- js/main.js
+++ js/main.js
@@ -1,18 +1,29 @@
-$(function(){
-	getBpi("USD");
+
+$(async function(){
+	 getBpi("USD");
   });
-  function getBpi(code)
-  { $.getJSON("https://api.coindesk.com/v1/bpi/currentprice.json",function(data)
-	 {
-		var desc=data.bpi[code].description;
-		var rate= data.bpi[code].rate;
-	   
+		  async function getBpi(code) {
+			const URL = 'https://api.coindesk.com/v1/bpi/currentprice.json'; // Will return a 404
+			const fetchResult = fetch(URL);
+			
+			const response = await fetchResult;
+		if (response.ok) {
+			  let data = await response.json();
+			  let desc=await data.bpi[code].description;
+		let rate=await data.bpi[code].rate;
+	//	const code=await data.bpi.code;
 		document.getElementById("desc").innerHTML=desc;
 		document.getElementById("code").innerHTML=code;
 		document.getElementById("rate").innerHTML=rate;
-  
-  
-  
-	 
-			 
-		   });}
+			 // console.log(data);
+		} else {
+			  throw Error(response.statusText);
+			  $(".err").show();  
+			  console.error(error);
+				//alert("Error connection");
+				
+			  }
+		  };
+		 
+		  getBpi('javascript');
+		  
